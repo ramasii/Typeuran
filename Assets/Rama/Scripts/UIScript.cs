e86a9f0d23e99d299c0afa2e873d37a216b46dc8
ramasii <amdramadhani221005@gmail.com>
2025-08-03 11:33:20 +0700

    pemasangan UI main menu

--- Assets/Rama/Scripts/UIScript.cs
+++ Assets/Rama/Scripts/UIScript.cs
@@ -3,20 +3,122 @@ using UnityEngine.SceneManagement;
 
 public class UIScript : MonoBehaviour
 {
+    private GameObject startButton;
     // Start is called before the first frame update
     void Start()
     {
-        
+        UIStarting();
+    }
+
+    private void UIStarting()
+    {
+        startButton = GameObject.Find("Start Button");
+        if (startButton != null) startButton.AddComponent<StartButton>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
     public void GoToScene(string sceneName)
     {
         SceneManager.LoadScene(sceneName);
+    }
+
+    public void DragStartButton()
+    {
+        if (startButton != null)
+        {
+            StartButton buttonScript = startButton.GetComponent<StartButton>();
+            if (buttonScript != null)
+            {
+                buttonScript.StartDrag();
+            }
+        }
+    }
+
+    public void EndDragStartButton()
+    {
+        if (startButton != null)
+        {
+            StartButton buttonScript = startButton.GetComponent<StartButton>();
+            if (buttonScript != null)
+            {
+                buttonScript.EndDrag();
+            }
+        }
+    }
+}
+
+public class StartButton : MonoBehaviour
+{
+    private float startingYPosition;
+    private bool isDragging = false;
+    private Transform rollingDoor;
+    private float rollingDoorStartingYPosition;
+    private GameObject judul;
+    private CanvasGroup judulCanvasGroup;
+
+    private void Start()
+    {
+        startingYPosition = transform.position.y;
+        rollingDoor = GameObject.Find("Rolling Door").transform;
+        rollingDoorStartingYPosition = rollingDoor.position.y;
+        judul = GameObject.Find("Judul");
+        if (judul != null) judulCanvasGroup = judul.GetComponent<CanvasGroup>();
+    }
+
+    private void Update()
+    {
+        Dragging();
+    }
+
+    private void Dragging()
+    {
+        Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        if (isDragging)
+        {
+            if (mousePosition.y > startingYPosition)
+            {
+                transform.position = new Vector3(transform.position.x, mousePosition.y, transform.position.z);
+                rollingDoor.position = new Vector3(rollingDoor.position.x, mousePosition.y + (rollingDoorStartingYPosition - startingYPosition), rollingDoor.position.z);
+            }
+            else
+            {
+                transform.position = new Vector3(transform.position.x, startingYPosition, transform.position.z);
+                rollingDoor.position = new Vector3(rollingDoor.position.x, rollingDoorStartingYPosition, rollingDoor.position.z);
+            }
+
+            judulCanvasGroup.alpha = transform.position.y / startingYPosition;
+        }
+        else
+        {
+            if (transform.position.y >= 3.5f)
+            {
+                transform.position += new Vector3(0f, 5f, 0f) * Time.deltaTime * 1;
+                rollingDoor.position += new Vector3(0f, 5f, 0f) * Time.deltaTime * 1;
+                if (transform.position.y >= 7f)
+                {
+                    SceneManager.LoadScene("GameLevel");
+                }
+            }else{
+                transform.position = Vector3.Lerp(transform.position, new Vector3(transform.position.x, startingYPosition, transform.position.z), Time.deltaTime * 5f);
+                rollingDoor.position = Vector3.Lerp(rollingDoor.position, new Vector3(rollingDoor.position.x, rollingDoorStartingYPosition, rollingDoor.position.z), Time.deltaTime * 5f);
+                judulCanvasGroup.alpha = Mathf.Lerp(judulCanvasGroup.alpha, 1f, Time.deltaTime * 5f);
+            }
+        }
+    }
+
+    public void StartDrag()
+    {
+        isDragging = true;
+    }
+
+    public void EndDrag()
+    {
+        isDragging = false;
+
     }
 }
